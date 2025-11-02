f577144d51fd67c8a54fbb15f2b33cc96e27a788
Elias <elias@falchus.com>
2025-11-02 22:30:07 +0100

    propertyutils

--- src/com/falchus/lib/minecraft/spigot/utils/PropertyUtils.java
+++ src/com/falchus/lib/minecraft/spigot/utils/PropertyUtils.java
@@ -2,6 +2,7 @@ package com.falchus.lib.minecraft.spigot.utils;
 
 import com.falchus.lib.minecraft.spigot.enums.Property;
 
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import net.minecraft.server.v1_8_R3.DedicatedServer;
 import net.minecraft.server.v1_8_R3.MinecraftServer;
@@ -18,7 +19,7 @@ public class PropertyUtils {
 		getPropertyManager().savePropertiesFile();
 	}
 	
-	public static void setProperty(Property property, Object value) {
+	public static void setProperty(@NonNull Property property, @NonNull Object value) {
 		getPropertyManager().setProperty(property.getKey(), value);
 		saveProperties();
 	}
