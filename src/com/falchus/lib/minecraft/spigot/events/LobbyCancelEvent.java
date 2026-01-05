b2121087041bd044977b966310cca0b26e66a47f
Elias <elias@falchus.com>
2026-01-05 18:59:57 +0100

    added LobbyCancelEvent#getEvent

--- src/com/falchus/lib/minecraft/spigot/events/LobbyCancelEvent.java
+++ src/com/falchus/lib/minecraft/spigot/events/LobbyCancelEvent.java
@@ -5,16 +5,20 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /**
  * Called when there's a event that should be cancelled in a Lobby.
  */
 @Getter
+@RequiredArgsConstructor
 public class LobbyCancelEvent extends Event implements Cancellable {
 
 	private static final HandlerList handlers = new HandlerList();
 	@Setter private boolean isCancelled;
+	
+	private final Event event;
 	
 	@Override
 	public HandlerList getHandlers() {
