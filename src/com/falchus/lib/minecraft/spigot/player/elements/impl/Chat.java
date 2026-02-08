c4b6e4d87fd4a013cdc241fd30b89eb9f49c58ef
Elias <elias@falchus.com>
2026-02-08 14:42:43 +0100

    fix Chat

--- src/com/falchus/lib/minecraft/spigot/player/elements/impl/Chat.java
+++ src/com/falchus/lib/minecraft/spigot/player/elements/impl/Chat.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Supplier;
 
 import org.bukkit.Bukkit;
@@ -19,8 +20,8 @@ import lombok.NonNull;
 
 public class Chat extends PlayerElement implements Listener {
 
-	private static final Map<Player, Supplier<String>> prefixSuppliers = new HashMap<>();
-	private static final Map<Player, String> lastPrefixes = new HashMap<>();
+	private static final Map<UUID, Supplier<String>> prefixSuppliers = new HashMap<>();
+	private static final Map<UUID, String> lastPrefixes = new HashMap<>();
 	
 	private static final List<Boolean> registered = new ArrayList<>();
 	
@@ -37,14 +38,14 @@ public class Chat extends PlayerElement implements Listener {
 			registered.add(true);
 		}
 		
-		prefixSuppliers.put(player, prefix);
+		prefixSuppliers.put(player.getUniqueId(), prefix);
 		
 		updateRunnable = () -> {
-			String lastPrefix = lastPrefixes.get(player);
-			String newPrefix = prefixSuppliers.get(player).get();
+			String lastPrefix = lastPrefixes.get(player.getUniqueId());
+			String newPrefix = prefixSuppliers.get(player.getUniqueId()).get();
 			
 			if (lastPrefix == null || !newPrefix.equals(lastPrefix)) {
-				lastPrefixes.put(player, newPrefix);
+				lastPrefixes.put(player.getUniqueId(), newPrefix);
 			}
 		};
 		update();
@@ -67,8 +68,8 @@ public class Chat extends PlayerElement implements Listener {
 	public void remove() {
 		super.remove();
 		
-		prefixSuppliers.remove(player);
-		lastPrefixes.remove(player);
+		prefixSuppliers.remove(player.getUniqueId());
+		lastPrefixes.remove(player.getUniqueId());
 		
 		if (registered.size() == 1 && registered.getFirst() == true) {
 			HandlerList.unregisterAll(this);
@@ -78,7 +79,7 @@ public class Chat extends PlayerElement implements Listener {
 	
 	@EventHandler(ignoreCancelled = true)
 	public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
-		String prefix = prefixSuppliers.get(event.getPlayer()).get();
+		String prefix = prefixSuppliers.get(event.getPlayer().getUniqueId()).get();
 		if (prefix == null) return;
 		
 		event.setFormat(prefix + "%2$s");
