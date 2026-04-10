ea0c1eb090306a58ded54377c8ef80de58789beb
Elias <elias@falchus.com>
2026-04-10 23:19:19 +0200

    Update AdventureUtils.java

--- src/com/falchus/lib/minecraft/utils/AdventureUtils.java
+++ src/com/falchus/lib/minecraft/utils/AdventureUtils.java
@@ -8,11 +8,11 @@ import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class AdventureUtils {
 
-    public static String toJson(@NonNull Component component) {
-        return GsonComponentSerializer.gson().serialize(component);
-    }
-
-    public static TextComponent legacy(@NonNull String input) {
-        return LegacyComponentSerializer.legacySection().deserialize(input);
-    }
+	public static String toJson(@NonNull Component component) {
+	    return GsonComponentSerializer.gson().serialize(component);
+	}
+	
+	public static TextComponent legacy(@NonNull String input) {
+		return LegacyComponentSerializer.legacySection().deserialize(input);
+	}
 }
