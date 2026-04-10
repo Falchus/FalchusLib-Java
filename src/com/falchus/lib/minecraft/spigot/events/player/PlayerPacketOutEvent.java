90a137ef58101195e9e5637cc08b31f5a640e5ff
Elias <elias@falchus.com>
2026-04-10 22:20:23 +0200

    Update PlayerPacketOutEvent.java

--- src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketOutEvent.java
+++ src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketOutEvent.java
@@ -1,16 +1,18 @@
 package com.falchus.lib.minecraft.spigot.events.player;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.Getter;
 import org.bukkit.entity.Player;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.Getter;
 
 /**
  * Called when a player sends a packet.
  */
 @Getter
 public class PlayerPacketOutEvent extends PlayerPacketEvent {
-
-    public PlayerPacketOutEvent(boolean async, Player player, PacketWrapper packet) {
-        super(async, player, packet);
-    }
+	
+	public PlayerPacketOutEvent(boolean async, Player player, PacketWrapper packet) {
+		super(async, player, packet);
+	}
 }
