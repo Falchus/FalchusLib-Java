05e44f2156e4b732cd3e65b1b41744ddafe92b9d
Elias <elias@falchus.com>
2026-04-10 23:05:34 +0200

    Update WrappedPacketOutWindowItems.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/WrappedPacketOutWindowItems.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/WrappedPacketOutWindowItems.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.window;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutWindowItems extends PacketUpdateWindowItems {
 
-    public WrappedPacketOutWindowItems(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutWindowItems",
-                        networkProtocolGame + "PacketPlayOutWindowItems"
-                )
-        );
-    }
+	public WrappedPacketOutWindowItems(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutWindowItems",
+				networkProtocolGame + "PacketPlayOutWindowItems"
+			)
+		);
+	}
 }
