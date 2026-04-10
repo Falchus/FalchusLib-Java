d985e07b3715b7d67622778ce18d6c3a934889b5
Elias <elias@falchus.com>
2026-04-10 22:51:16 +0200

    Update WrappedPacketOutRemoveEntityEffect.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/removeentityeffect/WrappedPacketOutRemoveEntityEffect.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/removeentityeffect/WrappedPacketOutRemoveEntityEffect.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.removeentityeffect;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutRemoveEntityEffect extends PacketRemoveEntityEffectWrapper {
 
-    WrappedPacketOutRemoveEntityEffect(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutRemoveEntityEffect",
-                        networkProtocolCommon + "PacketPlayOutRemoveEntityEffect"
-                )
-        );
-    }
+	WrappedPacketOutRemoveEntityEffect(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutRemoveEntityEffect",
+				networkProtocolCommon + "PacketPlayOutRemoveEntityEffect"
+			)
+		);
+	}
 }
