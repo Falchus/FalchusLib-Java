3043b22625fdbe0809cc57fa1a24b5730e9422b1
Elias <elias@falchus.com>
2026-04-10 23:06:06 +0200

    Update WrappedPacketInUseEntity.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/use/WrappedPacketInUseEntity.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/use/WrappedPacketInUseEntity.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.use;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInUseEntity extends PacketUseEntityWrapper {
 
-    public WrappedPacketInUseEntity(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInUseEntity",
-                        networkProtocolGame + "PacketPlayInUseEntity"
-                )
-        );
-    }
+	public WrappedPacketInUseEntity(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInUseEntity",
+				networkProtocolGame + "PacketPlayInUseEntity"
+			)
+		);
+	}
 }
