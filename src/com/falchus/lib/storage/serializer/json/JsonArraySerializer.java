ef891d68f3a4445792f360f20fe82d5315f51ad4
Elias <elias@falchus.com>
2026-04-10 23:21:11 +0200

    Update JsonArraySerializer.java

--- src/com/falchus/lib/storage/serializer/json/JsonArraySerializer.java
+++ src/com/falchus/lib/storage/serializer/json/JsonArraySerializer.java
@@ -3,26 +3,26 @@ package com.falchus.lib.storage.serializer.json;
 import org.json.simple.JSONArray;
 
 public abstract class JsonArraySerializer<T> extends JsonSerializer<T, JSONArray> {
-
-    @Override
-    public String serialize(T value) {
-        return to(value).toJSONString();
-    }
-
-    @Override
-    public T deserialize(String content) {
-        if (content == null || content.isBlank()) {
-            return from(new JSONArray());
-        }
-
-        try {
-            Object raw = parser.parse(content);
-            if (raw instanceof JSONArray json) {
-                return from(json);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return from(new JSONArray());
-    }
+	
+	@Override
+	public String serialize(T value) {
+		return to(value).toJSONString();
+	}
+	
+	@Override
+	public T deserialize(String content) {
+		if (content == null || content.isBlank()) {
+			return from(new JSONArray());
+		}
+		
+		try {
+			Object raw = parser.parse(content);
+			if (raw instanceof JSONArray json) {
+				return from(json);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return from(new JSONArray());
+	}
 }
