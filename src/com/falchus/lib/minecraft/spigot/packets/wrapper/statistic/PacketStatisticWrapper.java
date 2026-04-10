0b7e32c16545cf36af86cfe07a4a07989d5a57ae
Elias <elias@falchus.com>
2026-04-10 22:59:12 +0200

    Update PacketStatisticWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/statistic/PacketStatisticWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/statistic/PacketStatisticWrapper.java
@@ -1,13 +1,14 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.statistic;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-
 import java.util.Set;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
 
 abstract class PacketStatisticWrapper extends PacketWrapper {
 
-    PacketStatisticWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-    }
+	PacketStatisticWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+	}
 }
