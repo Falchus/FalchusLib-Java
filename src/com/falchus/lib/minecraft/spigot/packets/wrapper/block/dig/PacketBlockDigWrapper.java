c35331d6930e87e5df0d4a255cf94aeafa2d1ee4
Elias <elias@falchus.com>
2026-04-10 22:30:14 +0200

    Update PacketBlockDigWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/dig/PacketBlockDigWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/dig/PacketBlockDigWrapper.java
@@ -1,75 +1,76 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.block.dig;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketBlockDigWrapper extends PacketWrapper {
+	
+	Field pos;
+	Field direction;
+	Field action;
+	
+	PacketBlockDigWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		pos = getFirstField(
+			"pos",
+			"a"
+		);
+		direction = getFirstField(
+			"direction",
+			"b"
+		);
+		action = getFirstField(
+			"action",
+			"c"
+		);
+	}
 
-    Field pos;
-    Field direction;
-    Field action;
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(pos);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 
-    PacketBlockDigWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	/**
+	 * @return EnumDirection
+	 */
+	public Object getDirection() {
+		return getFieldValue(direction);
+	}
+	
+	/**
+	 * @param pos: EnumDirection
+	 */
+	public void setDirection(Object direction) {
+		setField(this.direction, direction);
+	}
 
-        pos = getFirstField(
-                "pos",
-                "a"
-        );
-        direction = getFirstField(
-                "direction",
-                "b"
-        );
-        action = getFirstField(
-                "action",
-                "c"
-        );
-    }
-
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(pos);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
-
-    /**
-     * @return EnumDirection
-     */
-    public Object getDirection() {
-        return getFieldValue(direction);
-    }
-
-    /**
-     * @param pos: EnumDirection
-     */
-    public void setDirection(Object direction) {
-        setField(this.direction, direction);
-    }
-
-    /**
-     * @return EnumPlayerDigType
-     */
-    public Object getAction() {
-        return getFieldValue(action);
-    }
-
-    /**
-     * @param pos: EnumPlayerDigType
-     */
-    public void setAction(Object action) {
-        setField(this.action, action);
-    }
+	/**
+	 * @return EnumPlayerDigType
+	 */
+	public Object getAction() {
+		return getFieldValue(action);
+	}
+	
+	/**
+	 * @param pos: EnumPlayerDigType
+	 */
+	public void setAction(Object action) {
+		setField(this.action, action);
+	}
 }
