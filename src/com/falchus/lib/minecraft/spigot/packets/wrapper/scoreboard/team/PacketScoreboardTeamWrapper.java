68208ab85282006c34a121963e4609c73f3c240d
Elias <elias@falchus.com>
2026-04-10 22:55:03 +0200

    Update PacketScoreboardTeamWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/team/PacketScoreboardTeamWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/team/PacketScoreboardTeamWrapper.java
@@ -1,58 +1,59 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.team;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketScoreboardTeamWrapper extends PacketWrapper {
+	
+	Field name;
+	Field players;
+	Field method;
 
-    Field name;
-    Field players;
-    Field method;
+	PacketScoreboardTeamWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		name = getFirstField(
+			"name",
+			"a"
+		);
+		players = getFirstField(
+			"players",
+			"g"
+		);
+		method = getFirstField(
+			"method",
+			"h"
+		);
+	}
 
-    PacketScoreboardTeamWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public String getName() {
+		return getFieldValue(name);
+	}
+	
+	public void setName(String name) {
+		setField(this.name, name);
+	}
 
-        name = getFirstField(
-                "name",
-                "a"
-        );
-        players = getFirstField(
-                "players",
-                "g"
-        );
-        method = getFirstField(
-                "method",
-                "h"
-        );
-    }
+	public Collection<String> getPlayers() {
+		return getFieldValue(players);
+	}
+	
+	public void setPlayers(Collection<String> players) {
+		setField(this.players, players);
+	}
 
-    public String getName() {
-        return getFieldValue(name);
-    }
-
-    public void setName(String name) {
-        setField(this.name, name);
-    }
-
-    public Collection<String> getPlayers() {
-        return getFieldValue(players);
-    }
-
-    public void setPlayers(Collection<String> players) {
-        setField(this.players, players);
-    }
-
-    public int getMethod() {
-        return getFieldValue(method);
-    }
-
-    public void setMethod(int method) {
-        setField(this.method, method);
-    }
+	public int getMethod() {
+		return getFieldValue(method);
+	}
+	
+	public void setMethod(int method) {
+		setField(this.method, method);
+	}
 }
