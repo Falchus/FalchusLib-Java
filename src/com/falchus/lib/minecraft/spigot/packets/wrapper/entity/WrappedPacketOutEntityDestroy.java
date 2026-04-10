4216b9fc027908c574285eb671fd8bba76792e4a
Elias <elias@falchus.com>
2026-04-10 22:39:04 +0200

    Update WrappedPacketOutEntityDestroy.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityDestroy.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityDestroy.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-
 import java.util.Set;
 
-public class WrappedPacketOutEntityDestroy extends PacketEntityWrapper {
+import lombok.NonNull;
 
-    WrappedPacketOutEntityDestroy(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityDestroy",
-                        networkProtocolGame + "PacketPlayOutEntityDestroy"
-                )
-        );
-    }
+public class WrappedPacketOutEntityDestroy extends PacketEntityWrapper {
+	
+	WrappedPacketOutEntityDestroy(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityDestroy",
+				networkProtocolGame + "PacketPlayOutEntityDestroy"
+			)
+		);
+	}
 }
