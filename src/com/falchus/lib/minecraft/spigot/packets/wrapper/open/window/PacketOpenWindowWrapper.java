b541edfc44c631db4fb7bb69e60f79cdd5bc4190
Elias <elias@falchus.com>
2026-04-10 22:49:16 +0200

    Update PacketOpenWindowWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/window/PacketOpenWindowWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/window/PacketOpenWindowWrapper.java
@@ -1,50 +1,51 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.open.window;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketOpenWindowWrapper extends PacketWrapper {
+	
+	Field containerId;
+	Field title;
 
-    Field containerId;
-    Field title;
+	PacketOpenWindowWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		containerId = getFirstField(
+			"containerId",
+			"a"
+		);
+		title = getFirstField(
+			"title",
+			"c"
+		);
+	}
 
-    PacketOpenWindowWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getContainerId() {
+		return getFieldValue(containerId);
+	}
+	
+	public void setContainerId(int containerId) {
+		setField(this.containerId, containerId);
+	}
 
-        containerId = getFirstField(
-                "containerId",
-                "a"
-        );
-        title = getFirstField(
-                "title",
-                "c"
-        );
-    }
-
-    public int getContainerId() {
-        return getFieldValue(containerId);
-    }
-
-    public void setContainerId(int containerId) {
-        setField(this.containerId, containerId);
-    }
-
-    /**
-     * @return IChatBaseComponent
-     */
-    public Object getTitle() {
-        return getFieldValue(title);
-    }
-
-    /**
-     * @param title: IChatBaseComponent
-     */
-    public void setTitle(Object title) {
-        setField(this.title, title);
-    }
+	/**
+	 * @return IChatBaseComponent
+	 */
+	public Object getTitle() {
+		return getFieldValue(title);
+	}
+	
+	/**
+	 * @param title: IChatBaseComponent
+	 */
+	public void setTitle(Object title) {
+		setField(this.title, title);
+	}
 }
