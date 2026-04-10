2dfd1e6f38c3dff52bb11f72cd4f4538dc92a4d2
Elias <elias@falchus.com>
2026-04-10 22:43:34 +0200

    Update PacketGameStateChangeWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/gamestatechange/PacketGameStateChangeWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/gamestatechange/PacketGameStateChangeWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.gamestatechange;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketGameStateChangeWrapper extends PacketWrapper {
+	
+	Field param;
 
-    Field param;
+	PacketGameStateChangeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		param = getFirstField(
+			"param",
+			"c"
+		);
+	}
 
-    PacketGameStateChangeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        param = getFirstField(
-                "param",
-                "c"
-        );
-    }
-
-    public float getParam() {
-        return getFieldValue(param);
-    }
-
-    public void setParam(float param) {
-        setField(this.param, param);
-    }
+	public float getParam() {
+		return getFieldValue(param);
+	}
+	
+	public void setParam(float param) {
+		setField(this.param, param);
+	}
 }
