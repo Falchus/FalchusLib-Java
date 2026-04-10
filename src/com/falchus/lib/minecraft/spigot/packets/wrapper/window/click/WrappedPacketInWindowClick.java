a6d97c0c6d7c8c45b07b2c85761a92f8e927c71f
Elias <elias@falchus.com>
2026-04-10 23:06:46 +0200

    Update WrappedPacketInWindowClick.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/window/click/WrappedPacketInWindowClick.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/window/click/WrappedPacketInWindowClick.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.window.click;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInWindowClick extends PacketWindowClickWrapper {
 
-    public WrappedPacketInWindowClick(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInWindowClick",
-                        networkProtocolGame + "PacketPlayInWindowClick"
-                )
-        );
-    }
+	public WrappedPacketInWindowClick(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInWindowClick",
+				networkProtocolGame + "PacketPlayInWindowClick"
+			)
+		);
+	}
 }
