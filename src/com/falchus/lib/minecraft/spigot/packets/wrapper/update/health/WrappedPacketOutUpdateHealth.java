04e75d07bb869729131cea474f698cfcb87060cf
Elias <elias@falchus.com>
2026-04-10 23:03:25 +0200

    Update WrappedPacketOutUpdateHealth.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/health/WrappedPacketOutUpdateHealth.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/health/WrappedPacketOutUpdateHealth.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.health;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutUpdateHealth extends PacketUpdateHealthWrapper {
 
-    public WrappedPacketOutUpdateHealth(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutUpdateHealth",
-                        networkProtocolGame + "PacketPlayOutUpdateHealth"
-                )
-        );
-    }
+	public WrappedPacketOutUpdateHealth(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutUpdateHealth",
+				networkProtocolGame + "PacketPlayOutUpdateHealth"
+			)
+		);
+	}
 }
