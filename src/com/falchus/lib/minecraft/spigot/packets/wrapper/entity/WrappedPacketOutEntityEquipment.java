2611832ac7bc5f85025181065bfa2a337817255f
Elias <elias@falchus.com>
2026-04-10 22:39:50 +0200

    Update WrappedPacketOutEntityEquipment.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityEquipment.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityEquipment.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-
 import java.util.Set;
 
-public class WrappedPacketOutEntityEquipment extends PacketEntityWrapper {
+import lombok.NonNull;
 
-    WrappedPacketOutEntityEquipment(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityEquipment",
-                        networkProtocolGame + "PacketPlayOutEntityEquipment"
-                )
-        );
-    }
+public class WrappedPacketOutEntityEquipment extends PacketEntityWrapper {
+	
+	WrappedPacketOutEntityEquipment(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityEquipment",
+				networkProtocolGame + "PacketPlayOutEntityEquipment"
+			)
+		);
+	}
 }
