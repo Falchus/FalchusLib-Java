2f6bf5d3157a236072bf8e802b6fdd3d43c01477
Elias <elias@falchus.com>
2026-04-10 22:35:42 +0200

    Update WrappedPacketInCloseWindow.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/closewindow/WrappedPacketInCloseWindow.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/closewindow/WrappedPacketInCloseWindow.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.closewindow;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInCloseWindow extends PacketCloseWindowWrapper {
 
-    public WrappedPacketInCloseWindow(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInCloseWindow",
-                        networkProtocolGame + "PacketPlayInCloseWindow"
-                )
-        );
-    }
+	public WrappedPacketInCloseWindow(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInCloseWindow",
+				networkProtocolGame + "PacketPlayInCloseWindow"
+			)
+		);
+	}
 }
