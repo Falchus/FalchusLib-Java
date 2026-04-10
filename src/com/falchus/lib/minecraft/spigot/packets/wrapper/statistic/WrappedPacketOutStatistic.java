099e9a7483ca896276bd8b07a358b633bb6c4ecd
Elias <elias@falchus.com>
2026-04-10 22:59:23 +0200

    Update WrappedPacketOutStatistic.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/statistic/WrappedPacketOutStatistic.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/statistic/WrappedPacketOutStatistic.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.statistic;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutStatistic extends PacketStatisticWrapper {
 
-    public WrappedPacketOutStatistic(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutStatistic",
-                        networkProtocolGame + "PacketPlayOutStatistic"
-                )
-        );
-    }
+	public WrappedPacketOutStatistic(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutStatistic",
+				networkProtocolGame + "PacketPlayOutStatistic"
+			)
+		);
+	}
 }
