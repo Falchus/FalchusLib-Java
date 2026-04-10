f32602dda8f02e94388af6f81b2bb2c94ff14798
Elias <elias@falchus.com>
2026-04-10 22:32:27 +0200

    Update PacketBlockPlaceWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/place/PacketBlockPlaceWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/place/PacketBlockPlaceWrapper.java
@@ -1,79 +1,80 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.block.place;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketBlockPlaceWrapper extends PacketWrapper {
+	
+	Field pos;
+	Field type;
+	Field data;
+	Field block;
+	
+	PacketBlockPlaceWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		pos = getFirstField(
+			"pos",
+			"a"
+		);
+		type = getFirstField(
+			"b0",
+			"b"
+		);
+		data = getFirstField(
+			"b1",
+			"c"
+		);
+		block = getFirstField(
+			"block",
+			"d"
+		);
+	}
 
-    Field pos;
-    Field type;
-    Field data;
-    Field block;
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(pos);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 
-    PacketBlockPlaceWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getType() {
+		return getFieldValue(type);
+	}
+	
+	public void setType(int type) {
+		setField(this.type, type);
+	}
 
-        pos = getFirstField(
-                "pos",
-                "a"
-        );
-        type = getFirstField(
-                "b0",
-                "b"
-        );
-        data = getFirstField(
-                "b1",
-                "c"
-        );
-        block = getFirstField(
-                "block",
-                "d"
-        );
-    }
+	public int getData() {
+		return getFieldValue(data);
+	}
+	
+	public void setData(int data) {
+		setField(this.data, data);
+	}
 
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(pos);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
-
-    public int getType() {
-        return getFieldValue(type);
-    }
-
-    public void setType(int type) {
-        setField(this.type, type);
-    }
-
-    public int getData() {
-        return getFieldValue(data);
-    }
-
-    public void setData(int data) {
-        setField(this.data, data);
-    }
-
-    public Object getBlock() {
-        return getFieldValue(block);
-    }
-
-    /**
-     * @param block: Block
-     */
-    public void setBlock(Object block) {
-        setField(this.block, block);
-    }
+	public Object getBlock() {
+		return getFieldValue(block);
+	}
+	
+	/**
+	 * @param block: Block
+	 */
+	public void setBlock(Object block) {
+		setField(this.block, block);
+	}
 }
