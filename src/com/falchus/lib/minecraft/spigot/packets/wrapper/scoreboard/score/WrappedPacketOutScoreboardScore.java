5a56307006f061496e4209e4dafa780a2bc81bc6
Elias <elias@falchus.com>
2026-04-10 22:54:38 +0200

    Update WrappedPacketOutScoreboardScore.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/score/WrappedPacketOutScoreboardScore.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/score/WrappedPacketOutScoreboardScore.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.score;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutScoreboardScore extends PacketScoreboardScoreWrapper {
 
-    WrappedPacketOutScoreboardScore(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutScoreboardScore",
-                        networkProtocolGame + "PacketPlayOutScoreboardScore"
-                )
-        );
-    }
+	WrappedPacketOutScoreboardScore(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutScoreboardScore",
+				networkProtocolGame + "PacketPlayOutScoreboardScore"
+			)
+		);
+	}
 }
