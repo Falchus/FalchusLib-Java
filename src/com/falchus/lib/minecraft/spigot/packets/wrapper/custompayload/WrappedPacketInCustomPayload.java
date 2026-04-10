ee74f6000ec218e92b80c77c550206f8d6be4914
Elias <elias@falchus.com>
2026-04-10 22:36:54 +0200

    Update WrappedPacketInCustomPayload.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/custompayload/WrappedPacketInCustomPayload.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/custompayload/WrappedPacketInCustomPayload.java
@@ -6,11 +6,11 @@ import lombok.NonNull;
 
 public class WrappedPacketInCustomPayload extends PacketCustomPayloadWrapper {
 
-    public WrappedPacketInCustomPayload(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInCustomPayload",
-                        networkProtocolCommon + "ServerboundCustomPayloadPacket"
-                )
-        );
-    }
+	public WrappedPacketInCustomPayload(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInCustomPayload",
+				networkProtocolCommon + "ServerboundCustomPayloadPacket"
+			)
+		);
+	}
 }
