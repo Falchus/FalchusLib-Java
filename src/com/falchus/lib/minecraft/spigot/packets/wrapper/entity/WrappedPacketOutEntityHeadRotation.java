478c3513bf66601ae256eba705c618b8a7d8b749
Elias <elias@falchus.com>
2026-04-10 22:40:04 +0200

    Update WrappedPacketOutEntityHeadRotation.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityHeadRotation.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityHeadRotation.java
@@ -1,34 +1,34 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutEntityHeadRotation extends PacketEntityWrapper {
+	
+	Field yHeadRot;
+	
+	WrappedPacketOutEntityHeadRotation(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityHeadRotation",
+				networkProtocolGame + "PacketPlayOutEntityHeadRotation"
+			)
+		);
+		
+		yHeadRot = getFirstField(
+			"yHeadRot",
+			"b"
+		);
+	}
 
-    Field yHeadRot;
-
-    WrappedPacketOutEntityHeadRotation(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityHeadRotation",
-                        networkProtocolGame + "PacketPlayOutEntityHeadRotation"
-                )
-        );
-
-        yHeadRot = getFirstField(
-                "yHeadRot",
-                "b"
-        );
-    }
-
-    public byte getYHeadRot() {
-        return getFieldValue(yHeadRot);
-    }
-
-    public void setYHeadRot(byte yHeadRot) {
-        setField(this.yHeadRot, yHeadRot);
-    }
+	public byte getYHeadRot() {
+		return getFieldValue(yHeadRot);
+	}
+	
+	public void setYHeadRot(byte yHeadRot) {
+		setField(this.yHeadRot, yHeadRot);
+	}
 }
