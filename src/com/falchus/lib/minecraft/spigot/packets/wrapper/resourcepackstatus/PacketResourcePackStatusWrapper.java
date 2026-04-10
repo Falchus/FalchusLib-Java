566c3991accde8765943300638550c4d2bd9bf8c
Elias <elias@falchus.com>
2026-04-10 22:51:35 +0200

    Update PacketResourcePackStatusWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/resourcepackstatus/PacketResourcePackStatusWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/resourcepackstatus/PacketResourcePackStatusWrapper.java
@@ -1,38 +1,39 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.resourcepackstatus;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketResourcePackStatusWrapper extends PacketWrapper {
+	
+	Field status;
 
-    Field status;
+	PacketResourcePackStatusWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		status = getFirstField(
+			"status",
+			"action",
+			"b"
+		);
+	}
 
-    PacketResourcePackStatusWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        status = getFirstField(
-                "status",
-                "action",
-                "b"
-        );
-    }
-
-    /**
-     * @return PacketPlayInResourcePackStatus$EnumResourcePackStatus
-     */
-    public Object getStatus() {
-        return getFieldValue(status);
-    }
-
-    /**
-     * @param status: PacketPlayInResourcePackStatus$EnumResourcePackStatus
-     */
-    public void setStatus(Object status) {
-        setField(this.status, status);
-    }
+	/**
+	 * @return PacketPlayInResourcePackStatus$EnumResourcePackStatus
+	 */
+	public Object getStatus() {
+		return getFieldValue(status);
+	}
+	
+	/**
+	 * @param status: PacketPlayInResourcePackStatus$EnumResourcePackStatus
+	 */
+	public void setStatus(Object status) {
+		setField(this.status, status);
+	}
 }
