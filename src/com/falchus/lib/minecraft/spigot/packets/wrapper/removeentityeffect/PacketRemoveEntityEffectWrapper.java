d5b0693cc0ce751f0425e9c059dae8567ca79933
Elias <elias@falchus.com>
2026-04-10 22:51:05 +0200

    Update PacketRemoveEntityEffectWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/removeentityeffect/PacketRemoveEntityEffectWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/removeentityeffect/PacketRemoveEntityEffectWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.removeentityeffect;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketRemoveEntityEffectWrapper extends PacketWrapper {
+	
+	Field entityId;
 
-    Field entityId;
+	PacketRemoveEntityEffectWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		entityId = getFirstField(
+			"entityId",
+			"a"
+		);
+	}
 
-    PacketRemoveEntityEffectWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        entityId = getFirstField(
-                "entityId",
-                "a"
-        );
-    }
-
-    public int getEntityId() {
-        return getFieldValue(entityId);
-    }
-
-    public void setEntityId(int entityId) {
-        setField(this.entityId, entityId);
-    }
+	public int getEntityId() {
+		return getFieldValue(entityId);
+	}
+	
+	public void setEntityId(int entityId) {
+		setField(this.entityId, entityId);
+	}
 }
