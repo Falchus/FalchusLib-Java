17ad0f4a6bb07aadf64fdf963d08a9a9a8e8abb7
Elias <elias@falchus.com>
2026-04-10 23:24:12 +0200

    Update Dummy.java

--- src/com/falchus/lib/utils/reflection/Dummy.java
+++ src/com/falchus/lib/utils/reflection/Dummy.java
@@ -2,8 +2,7 @@ package com.falchus.lib.utils.reflection;
 
 public final class Dummy {
 
-    public static final Dummy instance = new Dummy();
-
-    private Dummy() {
-    }
+	public static final Dummy instance = new Dummy();
+	
+	private Dummy() {}
 }
