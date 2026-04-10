8d7ec30a78affe21de4ce8efb8b6c54dbbb55ff7
Elias <elias@falchus.com>
2026-04-10 23:05:56 +0200

    Update PacketUseEntityWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/use/PacketUseEntityWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/use/PacketUseEntityWrapper.java
@@ -1,47 +1,48 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.use;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUseEntityWrapper extends PacketWrapper {
+	
+	Field entityId;
+	Field action;
 
-    Field entityId;
-    Field action;
+	PacketUseEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		entityId = getFirstField(
+			"entityId",
+			"a"
+		);
+		action = getField("action");
+	}
 
-    PacketUseEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getEntityId() {
+		return getFieldValue(entityId);
+	}
+	
+	public void setEntityId(int entityId) {
+		setField(this.entityId, entityId);
+	}
 
-        entityId = getFirstField(
-                "entityId",
-                "a"
-        );
-        action = getField("action");
-    }
-
-    public int getEntityId() {
-        return getFieldValue(entityId);
-    }
-
-    public void setEntityId(int entityId) {
-        setField(this.entityId, entityId);
-    }
-
-    /**
-     * @return PacketPlayInUseEntity$EnumEntityUseAction
-     */
-    public Object getAction() {
-        return getFieldValue(action);
-    }
-
-    /**
-     * @param action: PacketPlayInUseEntity$EnumEntityUseAction
-     */
-    public void setAction(Object action) {
-        setField(this.action, action);
-    }
+	/**
+	 * @return PacketPlayInUseEntity$EnumEntityUseAction
+	 */
+	public Object getAction() {
+		return getFieldValue(action);
+	}
+	
+	/**
+	 * @param action: PacketPlayInUseEntity$EnumEntityUseAction
+	 */
+	public void setAction(Object action) {
+		setField(this.action, action);
+	}
 }
