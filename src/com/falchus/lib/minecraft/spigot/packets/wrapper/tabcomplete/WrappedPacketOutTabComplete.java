68e3038fe77ed410ab702f44e2b4564c012b4e3b
Elias <elias@falchus.com>
2026-04-10 23:00:42 +0200

    Update WrappedPacketOutTabComplete.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/tabcomplete/WrappedPacketOutTabComplete.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/tabcomplete/WrappedPacketOutTabComplete.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.tabcomplete;
 
-import lombok.NonNull;
-
 import java.util.Set;
 
-public class WrappedPacketOutTabComplete extends PacketTabCompleteWrapper {
+import lombok.NonNull;
 
-    public WrappedPacketOutTabComplete(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutTabComplete",
-                        networkProtocolGame + "PacketPlayOutTabComplete"
-                )
-        );
-    }
+public class WrappedPacketOutTabComplete extends PacketTabCompleteWrapper {
+	
+	public WrappedPacketOutTabComplete(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutTabComplete",
+				networkProtocolGame + "PacketPlayOutTabComplete"
+			)
+		);
+	}
 }
