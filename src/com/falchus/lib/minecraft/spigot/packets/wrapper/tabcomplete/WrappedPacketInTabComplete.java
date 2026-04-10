3dddc04b1897e9d93b78779dbfec0ac2a55243dd
Elias <elias@falchus.com>
2026-04-10 23:00:10 +0200

    Update WrappedPacketInTabComplete.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/tabcomplete/WrappedPacketInTabComplete.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/tabcomplete/WrappedPacketInTabComplete.java
@@ -1,34 +1,34 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.tabcomplete;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketInTabComplete extends PacketTabCompleteWrapper {
 
-    Field command;
+	Field command;
+	
+	public WrappedPacketInTabComplete(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInTabComplete",
+				networkProtocolGame + "PacketPlayInTabComplete"
+			)
+		);
+		
+		command = getFirstField(
+			"command",
+			"b"
+		);
+	}
 
-    public WrappedPacketInTabComplete(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInTabComplete",
-                        networkProtocolGame + "PacketPlayInTabComplete"
-                )
-        );
-
-        command = getFirstField(
-                "command",
-                "b"
-        );
-    }
-
-    public String getCommand() {
-        return getFieldValue(command);
-    }
-
-    public void setCommand(int command) {
-        setField(this.command, command);
-    }
+	public String getCommand() {
+		return getFieldValue(command);
+	}
+	
+	public void setCommand(int command) {
+		setField(this.command, command);
+	}
 }
