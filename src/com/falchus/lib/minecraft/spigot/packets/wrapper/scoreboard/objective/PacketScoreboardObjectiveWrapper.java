a7e138cc2d82eb095dd521f837b6b30834bd16d8
Elias <elias@falchus.com>
2026-04-10 22:53:40 +0200

    Update PacketScoreboardObjectiveWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/objective/PacketScoreboardObjectiveWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/objective/PacketScoreboardObjectiveWrapper.java
@@ -1,63 +1,64 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.objective;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketScoreboardObjectiveWrapper extends PacketWrapper {
+	
+	Field objectiveName;
+	Field renderType;
+	Field method;
 
-    Field objectiveName;
-    Field renderType;
-    Field method;
+	PacketScoreboardObjectiveWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		objectiveName = getFirstField(
+			"objectiveName",
+			"a"
+		);
+		renderType = getFirstField(
+			"renderType",
+			"c"
+		);
+		method = getFirstField(
+			"method",
+			"d"
+		);
+	}
 
-    PacketScoreboardObjectiveWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public String getObjectiveName() {
+		return getFieldValue(objectiveName);
+	}
+	
+	public void setObjectiveName(String objectiveName) {
+		setField(this.objectiveName, objectiveName);
+	}
 
-        objectiveName = getFirstField(
-                "objectiveName",
-                "a"
-        );
-        renderType = getFirstField(
-                "renderType",
-                "c"
-        );
-        method = getFirstField(
-                "method",
-                "d"
-        );
-    }
+	/**
+	 * @return IScoreboardCriteria$EnumScoreboardHealthDisplay
+	 */
+	public Object getRenderType() {
+		return getFieldValue(renderType);
+	}
+	
+	/**
+	 * @param renderType: IScoreboardCriteria$EnumScoreboardHealthDisplay
+	 */
+	public void setRenderType(Object renderType) {
+		setField(this.renderType, renderType);
+	}
 
-    public String getObjectiveName() {
-        return getFieldValue(objectiveName);
-    }
-
-    public void setObjectiveName(String objectiveName) {
-        setField(this.objectiveName, objectiveName);
-    }
-
-    /**
-     * @return IScoreboardCriteria$EnumScoreboardHealthDisplay
-     */
-    public Object getRenderType() {
-        return getFieldValue(renderType);
-    }
-
-    /**
-     * @param renderType: IScoreboardCriteria$EnumScoreboardHealthDisplay
-     */
-    public void setRenderType(Object renderType) {
-        setField(this.renderType, renderType);
-    }
-
-    public int getMethod() {
-        return getFieldValue(method);
-    }
-
-    public void setMethod(int method) {
-        setField(this.method, method);
-    }
+	public int getMethod() {
+		return getFieldValue(method);
+	}
+	
+	public void setMethod(int method) {
+		setField(this.method, method);
+	}
 }
