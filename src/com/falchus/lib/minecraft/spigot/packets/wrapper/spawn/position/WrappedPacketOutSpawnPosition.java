de622a964ac167f0797e56c8591f12ed91769fd7
Elias <elias@falchus.com>
2026-04-10 22:58:12 +0200

    Update WrappedPacketOutSpawnPosition.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/position/WrappedPacketOutSpawnPosition.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/position/WrappedPacketOutSpawnPosition.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spawn.position;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutSpawnPosition extends PacketSpawnPositionWrapper {
 
-    public WrappedPacketOutSpawnPosition(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutSpawnPosition",
-                        networkProtocolGame + "PacketPlayOutSpawnPosition"
-                )
-        );
-    }
+	public WrappedPacketOutSpawnPosition(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutSpawnPosition",
+				networkProtocolGame + "PacketPlayOutSpawnPosition"
+			)
+		);
+	}
 }
