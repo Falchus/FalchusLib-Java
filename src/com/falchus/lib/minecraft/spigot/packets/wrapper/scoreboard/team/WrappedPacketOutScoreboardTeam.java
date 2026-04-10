9f35a6e4e232d40da302382528572c518c1df65c
Elias <elias@falchus.com>
2026-04-10 22:55:15 +0200

    Update WrappedPacketOutScoreboardTeam.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/team/WrappedPacketOutScoreboardTeam.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/team/WrappedPacketOutScoreboardTeam.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.team;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutScoreboardTeam extends PacketScoreboardTeamWrapper {
 
-    WrappedPacketOutScoreboardTeam(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutScoreboardTeam",
-                        networkProtocolGame + "PacketPlayOutScoreboardTeam"
-                )
-        );
-    }
+	WrappedPacketOutScoreboardTeam(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutScoreboardTeam",
+				networkProtocolGame + "PacketPlayOutScoreboardTeam"
+			)
+		);
+	}
 }
