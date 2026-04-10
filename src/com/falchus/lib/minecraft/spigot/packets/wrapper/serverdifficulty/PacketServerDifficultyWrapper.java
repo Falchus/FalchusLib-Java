c272abd8e369f4c881b95b9e76a62011a1a4bfab
Elias <elias@falchus.com>
2026-04-10 22:55:33 +0200

    Update PacketServerDifficultyWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/serverdifficulty/PacketServerDifficultyWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/serverdifficulty/PacketServerDifficultyWrapper.java
@@ -1,50 +1,51 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.serverdifficulty;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketServerDifficultyWrapper extends PacketWrapper {
+	
+	Field difficulty;
+	Field locked;
 
-    Field difficulty;
-    Field locked;
+	PacketServerDifficultyWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		difficulty = getFirstField(
+			"difficulty",
+			"a"
+		);
+		locked = getFirstField(
+			"locked",
+			"b"
+		);
+	}
 
-    PacketServerDifficultyWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	/**
+	 * @return EnumDifficulty
+	 */
+	public Object getDifficulty() {
+		return getFieldValue(difficulty);
+	}
+	
+	/**
+	 * @param difficulty: EnumDifficulty
+	 */
+	public void setDifficulty(Object difficulty) {
+		setField(this.difficulty, difficulty);
+	}
 
-        difficulty = getFirstField(
-                "difficulty",
-                "a"
-        );
-        locked = getFirstField(
-                "locked",
-                "b"
-        );
-    }
-
-    /**
-     * @return EnumDifficulty
-     */
-    public Object getDifficulty() {
-        return getFieldValue(difficulty);
-    }
-
-    /**
-     * @param difficulty: EnumDifficulty
-     */
-    public void setDifficulty(Object difficulty) {
-        setField(this.difficulty, difficulty);
-    }
-
-    public boolean isLocked() {
-        return getFieldValue(locked);
-    }
-
-    public void setLocked(boolean locked) {
-        setField(this.locked, locked);
-    }
+	public boolean isLocked() {
+		return getFieldValue(locked);
+	}
+	
+	public void setLocked(boolean locked) {
+		setField(this.locked, locked);
+	}
 }
