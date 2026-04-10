e628efdb46e7d3f92881b86a99025df1033e6898
Elias <elias@falchus.com>
2026-04-10 22:52:34 +0200

    Update PacketRespawnWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/respawn/PacketRespawnWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/respawn/PacketRespawnWrapper.java
@@ -1,37 +1,38 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.respawn;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketRespawnWrapper extends PacketWrapper {
+	
+	Field levelType;
 
-    Field levelType;
+	PacketRespawnWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		levelType = getFirstField(
+			"levelType",
+			"d"
+		);
+	}
 
-    PacketRespawnWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        levelType = getFirstField(
-                "levelType",
-                "d"
-        );
-    }
-
-    /**
-     * @return WorldType
-     */
-    public Object getLevelType() {
-        return getFieldValue(levelType);
-    }
-
-    /**
-     * @param levelType: WorldType
-     */
-    public void setLevelType(Object levelType) {
-        setField(this.levelType, levelType);
-    }
+	/**
+	 * @return WorldType
+	 */
+	public Object getLevelType() {
+		return getFieldValue(levelType);
+	}
+	
+	/**
+	 * @param levelType: WorldType
+	 */
+	public void setLevelType(Object levelType) {
+		setField(this.levelType, levelType);
+	}
 }
