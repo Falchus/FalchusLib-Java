6316d3fcdf3b671926e06628c3f1bb428d4e97af
Elias <elias@falchus.com>
2026-04-10 23:02:39 +0200

    Update PacketUpdateAttributesWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/attributes/PacketUpdateAttributesWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/attributes/PacketUpdateAttributesWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.attributes;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUpdateAttributesWrapper extends PacketWrapper {
+	
+	Field entityId;
 
-    Field entityId;
+	PacketUpdateAttributesWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		entityId = getFirstField(
+			"entityId",
+			"a"
+		);
+	}
 
-    PacketUpdateAttributesWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        entityId = getFirstField(
-                "entityId",
-                "a"
-        );
-    }
-
-    public void setPos(int entityId) {
-        setField(this.entityId, entityId);
-    }
-
-    public int getEntityId() {
-        return getFieldValue(entityId);
-    }
+	public int getEntityId() {
+		return getFieldValue(entityId);
+	}
+	
+	public void setPos(int entityId) {
+		setField(this.entityId, entityId);
+	}
 }
