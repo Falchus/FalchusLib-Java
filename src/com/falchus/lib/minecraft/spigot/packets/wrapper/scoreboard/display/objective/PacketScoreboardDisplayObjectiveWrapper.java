4418a2cdae45198bafe6c815da8edaea9e2f4fe4
Elias <elias@falchus.com>
2026-04-10 22:53:00 +0200

    Update PacketScoreboardDisplayObjectiveWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/display/objective/PacketScoreboardDisplayObjectiveWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/display/objective/PacketScoreboardDisplayObjectiveWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.display.objective;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketScoreboardDisplayObjectiveWrapper extends PacketWrapper {
+	
+	Field objectiveName;
 
-    Field objectiveName;
+	PacketScoreboardDisplayObjectiveWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		objectiveName = getFirstField(
+			"objectiveName",
+			"b"
+		);
+	}
 
-    PacketScoreboardDisplayObjectiveWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        objectiveName = getFirstField(
-                "objectiveName",
-                "b"
-        );
-    }
-
-    public String getObjectiveName() {
-        return getFieldValue(objectiveName);
-    }
-
-    public void setObjectiveName(String objectiveName) {
-        setField(this.objectiveName, objectiveName);
-    }
+	public String getObjectiveName() {
+		return getFieldValue(objectiveName);
+	}
+	
+	public void setObjectiveName(String objectiveName) {
+		setField(this.objectiveName, objectiveName);
+	}
 }
