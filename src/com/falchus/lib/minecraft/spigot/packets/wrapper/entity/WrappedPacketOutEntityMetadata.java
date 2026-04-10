63110bb7f57397cda420a5ec2e98d902eff92f34
Elias <elias@falchus.com>
2026-04-10 22:40:14 +0200

    Update WrappedPacketOutEntityMetadata.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityMetadata.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityMetadata.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-
 import java.util.Set;
 
-public class WrappedPacketOutEntityMetadata extends PacketEntityWrapper {
+import lombok.NonNull;
 
-    WrappedPacketOutEntityMetadata(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityMetadata",
-                        networkProtocolGame + "PacketPlayOutEntityMetadata"
-                )
-        );
-    }
+public class WrappedPacketOutEntityMetadata extends PacketEntityWrapper {
+	
+	WrappedPacketOutEntityMetadata(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityMetadata",
+				networkProtocolGame + "PacketPlayOutEntityMetadata"
+			)
+		);
+	}
 }
