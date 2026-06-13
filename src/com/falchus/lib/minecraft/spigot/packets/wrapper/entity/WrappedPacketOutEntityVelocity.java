978da1ec7453867d3df49db23b30bbf0c6d50ff9
Elias <elias@falchus.com>
2026-06-13 19:33:21 +0200

    add x,y,z to WrappedPacketOutEntityVelocity

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityVelocity.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityVelocity.java
@@ -1,15 +1,58 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
+import java.lang.reflect.Field;
 import java.util.Set;
 
 import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
+@FieldDefaults(makeFinal = true)
 public class WrappedPacketOutEntityVelocity extends PacketEntityWrapper {
+	
+	Field x;
+	Field y;
+	Field z;
 	
 	private WrappedPacketOutEntityVelocity(@NonNull Object handle) {
 		super(handle, Set.of(
 			version.getPackageNms() + "PacketPlayOutEntityVelocity",
 			networkProtocolGame + "PacketPlayOutEntityVelocity"
 		));
+		x = getFirstField(
+			"xa",
+			"b"
+		);
+		y = getFirstField(
+			"ya",
+			"c"
+		);
+		z = getFirstField(
+			"za",
+			"d"
+		);
+	}
+	
+	public int getX() {
+		return getFieldValue(x);
+	}
+	
+	public void setX(int x) {
+		setField(this.x, x);
+	}
+	
+	public int getY() {
+		return getFieldValue(y);
+	}
+	
+	public void setY(int y) {
+		setField(this.y, y);
+	}
+	
+	public int getZ() {
+		return getFieldValue(z);
+	}
+	
+	public void setZ(int z) {
+		setField(this.z, z);
 	}
 }
